a372968dc2ee1a32d73b16a87f78aac779ae093a
Songtao Dai <songtao@attitudetech.ie>
2017-01-17 10:00:26 +0000

    Validation Code Done

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -5,12 +5,43 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+
+require_once 'resources/org/code/Code.class.php';
 
 class LoginController extends CommonController
 {
-    //
+    //admin login template
     public function login(){
 
-        return view('admin.login');
+        if($input = Input::all()){
+
+            $user_input_code = strtoupper($input['code']);
+            $system_code = $this->getCode();
+
+            if($user_input_code != $system_code){
+                return back()->with('msg','Incorrect Validation Code');
+            }else{
+                dd('Validation Successful');
+            }
+
+        }else{
+            return view('admin.login');
+        }
+    }
+
+    //make validation code
+    public function code(){
+
+        $code = new \Code();
+        $code -> make();
+    }
+
+    //get validation code
+    public function getCode(){
+
+        $code = new \Code();
+        $val = $code ->get();
+        return $val;
     }
 }
